6780c1d51402073a6298a118369885f07743cf4d
Bruno <bruno.eng.proj@hotmail.com>
2024-12-06 09:15:02 -0300

    aula 04

---
+++ aula_04/lista_dicionario.py
@@ -0,0 +1,12 @@
+produto_01:str = "sapato"
+produto_02:str = "camisa"
+produto_03:str = "calça"
+
+produto:list = []
+
+produto.append(produto_01)
+produto.append(produto_02)
+produto.append(produto_03)
+produto.pop()
+
+print(produto)
---
+++ aula_04/main.py
@@ -0,0 +1,5 @@
+idade:int =30
+altura:float=1.90
+nome:str="Joao"
+estuadante:bool=True
+ 
