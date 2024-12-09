265dee8e785c939a6c22c3766790ac7f0d967465
Bruno <bruno.eng.proj@hotmail.com>
2024-12-09 16:23:21 -0300

    aula_07

---
+++ aula_07/exemplo.py
@@ -0,0 +1,12 @@
+valorx = 10
+valory = 5
+
+
+def soma(valor_1:float,  valor_2:float) -> float:
+    valor_03 = valor_1 + valor_2
+    return valor_03
+
+
+
+valor = soma(valor_1=valorx, valor_2=valory)
+print(valor)
